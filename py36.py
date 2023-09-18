bccb48414df01d3c8b40e97f1e0da0640c0cb041
Ruslan Isubilov <primat777@gmail.com>
2023-09-18 20:43:06 +0300

    замена csv файла, на sqllite db.

--- py36.py
+++ py36.py
@@ -3,7 +3,17 @@ import requests
 import csv
 import telebot
 import time
-GROUP_ID=-1001383125195
+import sqlite3
+
+con = sqlite3.connect('py36.db')
+cur = con.cursor()
+# Create table
+#cur.execute('''CREATE TABLE posts
+#               (picurl text)''')
+
+
+
+GROUP_ID=-1001255740387
 TOKEN=""
 bot = telebot.TeleBot(TOKEN)
 
@@ -13,19 +23,24 @@ pat='<td[ ]align="center"[ ]class="medium">.+?<a href.+?>(.+?)</a>.+?Польз�
 pat2='<img.+?src="(http://rangefinder.ru/glr/data/\d+/.+?)"'
 GalleryList=re.findall(pat,r.text,re.DOTALL)
 
-with open('current36.csv', newline='') as csvfile:
-    old36 = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
-print(old36[0])
-
-with open('current36.csv', 'w',newline='') as f: 
-    write = csv.writer(f)
-    write.writerow([x[2] for x in GalleryList])
-print(len(GalleryList))
+##with open('current36.csv', newline='') as csvfile: 
+##    old36 = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
+###print(old36[0])
+##if len(GalleryList) <36:exit;
+##
+##with open('current36.csv', 'w',newline='') as f: 
+##    write = csv.writer(f)
+##    write.writerow([x[2] for x in GalleryList])
+##print(len(GalleryList))
 
 
 for r in GalleryList:
-    print(r[0],r[1],r[2])
-    if r[2] not in old36[0]:
+    #print(r[0],r[1],r[2])
+    cur.execute("SELECT picurl FROM posts WHERE picurl = ?", (r[2],))
+    data=cur.fetchall()
+    if len(data)==0:
+        cur.execute("INSERT INTO posts values(?)",(r[2],))
+        con.commit()
         print(r[2])
         req=requests.get(r[2])
         ResUrl=[x for x in re.findall(pat2,req.text) if "thumb" not in x]
@@ -33,5 +48,9 @@ for r in GalleryList:
         #bot.send_message(GROUP_ID,'\r\n'.join([r[0]+" ("+ResUrl[0]+")",r[1]]))
         medias=[telebot.types.InputMediaPhoto(x,'\r\n'.join(r))for x in ResUrl]
         bot.send_media_group(GROUP_ID,medias)
-        time.sleep(3)
+        #time.sleep(11)
 
+
+# Save (commit) the changes
+con.commit()
+cur.clsoe()
